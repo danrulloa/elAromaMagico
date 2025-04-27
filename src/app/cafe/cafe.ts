88db2ce79b69f96a54a7848aac2a80723bd206f2
danrulloa <d.ulloa@uniandes.edu.co>
2025-04-27 03:05:35 -0500

    Ajuste de todos los servicios ajuste de diseños, estilos, Bootstrap, CSS ajuste de imagen

--- src/app/cafe/cafe.ts
+++ src/app/cafe/cafe.ts
@@ -1,9 +1,11 @@
 export class Cafe {
    public constructor(
+    public id: string,
     public nombre: string,
     public tipo: string,
     public region: string,
    ){
+        this.id = id;
         this.nombre = nombre;
         this.tipo = tipo;
         this.region = region;
